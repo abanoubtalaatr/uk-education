cd836e84ad1b7cb95b6e3ea3c8314c5d8a4dcc12
abanoubtalaatr <abanoubtalaat555@gmail.com>
2024-11-09 01:22:54 -0700

    WIP

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -2,17 +2,13 @@
 
 namespace App\Nova;
 
-use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
+use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\MorphToMany;
 use Laravel\Nova\Fields\Password;
-use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use Maatwebsite\LaravelNovaExcel\Actions\DownloadExcel;
 
 class User extends Resource
 {
@@ -40,26 +36,6 @@ class User extends Resource
     ];
 
     /**
-     * Get the displayable label of the resource.
-     *
-     * @return string
-     */
-    public static function label()
-    {
-        return __('Students');
-    }
-
-    /**
-     * Get the displayable singular label of the resource.
-     *
-     * @return string
-     */
-    public static function singularLabel()
-    {
-        return __('User');
-    }
-
-    /**
      * Get the fields displayed by the resource.
      *
      * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
@@ -68,64 +44,24 @@ class User extends Resource
     public function fields(NovaRequest $request)
     {
         return [
-            ID::make(__('ID'), 'id')->sortable(),
+            ID::make()->sortable(),
 
-            Images::make(__('Profile Picture'), 'profile_picture')
-                ->withResponsiveImages()
-                ->showOnPreview()   
-                ->nullable()
-                ->mustCrop(),
+            Gravatar::make()->maxWidth(50),
 
-                Images::make(__('Exam Confirmation Email'), 'exam_confirmation_email')
-                ->withResponsiveImages()
-                ->showOnPreview()   
-                ->nullable()
-                ->mustCrop(),
-
-                Images::make(__('First Image Exam Confirmation'), 'exam_confirmation_one')
-                ->withResponsiveImages()
-                ->showOnPreview()   
-                ->nullable()
-                ->mustCrop(),
-
-                Images::make(__('Second Image Exam Confirmation'), 'exam_confirmation_two')
-                ->withResponsiveImages()
-                ->showOnPreview()   
-                ->nullable()
-                ->mustCrop(),
-
-            Text::make(__('Name'), 'name')
+            Text::make('Name')
                 ->sortable()
                 ->rules('required', 'max:255'),
 
-            Text::make(__('Email'), 'email')
+            Text::make('Email')
                 ->sortable()
                 ->rules('required', 'email', 'max:254')
                 ->creationRules('unique:users,email')
                 ->updateRules('unique:users,email,{{resourceId}}'),
 
-            Password::make(__('Password'), 'password')
+            Password::make('Password')
                 ->onlyOnForms()
                 ->creationRules('required', Rules\Password::defaults())
                 ->updateRules('nullable', Rules\Password::defaults()),
-
-
-            Text::make(__('Phone'), 'mobile')
-                ->rules('required'),
-
-                Text::make(__("Whats app Number"), 'whats_app_number')
-                ->rules('required'),
-
-
-                Text::make(__("GMC Number"), 'gmc_number')
-                ->rules('required'),
-
-                DateTime::make(__("Exam Date"), 'exam_date')
-                ->rules('required'),
-
-
-
-            MorphToMany::make('Roles', 'roles', \Sereny\NovaPermissions\Nova\Role::class),
         ];
     }
 
@@ -170,8 +106,6 @@ class User extends Resource
      */
     public function actions(NovaRequest $request)
     {
-        return [
-            DownloadExcel::make()->withHeadings(),
-        ];
+        return [];
     }
 }
