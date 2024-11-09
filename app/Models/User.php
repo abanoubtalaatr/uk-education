3488f32444b6b3693d41bbdc3549725022decfd3
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-11-09 12:41:54 +0200

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
+use Spatie\Image\Enums\Fit;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Notifications\Notifiable;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Sanctum\HasApiTokens;
-use Spatie\Image\Enums\Fit;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements MustVerifyEmail, HasMedia
 {
@@ -130,11 +131,10 @@ class User extends Authenticatable implements MustVerifyEmail, HasMedia
     public function examConfirmationEmail(): Attribute
     {
         return Attribute::make(
-            // get: fn($value) => $this->getFirstMediaUrl('exam_confirmation_email')
-            get: fn($value) => $this->getFirstMediaUrl('exam_confirmation_email')
-
+            get: fn($value) => url(Storage::url($this->getFirstMediaUrl('exam_confirmation_email')))
         );
     }
+
 
     public function examConfirmationOne(): Attribute
     {
